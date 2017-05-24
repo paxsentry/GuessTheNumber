8b5f8c2e54b881eb2b00f2988b7ab896a50f1d28
PaxSentry <paxsentry@gmail.com>
2017-05-24 19:23:59 +0100

    Saving to GitHub

---
+++ Assets/Script/GuessTheNumberGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessTheNumberGame : MonoBehaviour
+{
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int usersGuess;
+
+    // Use this for initialization
+    void Start()
+    {
+        guessNumber = Random.Range(0, 100);
+    }
+
+    public void CheckGuess()
+    {
+        usersGuess = int.Parse(input.text);
+
+        if (usersGuess == guessNumber)
+        {
+            infoText.text = "You guessed rigth the number! Great!";
+        }
+        else if (usersGuess > guessNumber)
+        {
+            infoText.text = "Your number is greater than the guess number.";
+        }
+        else if (usersGuess < guessNumber)
+        {
+            infoText.text = "You number is lower than the guess number.";
+        }
+
+        input.text = "";
+    }
+}
